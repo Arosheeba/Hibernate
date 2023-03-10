e6434c7cc8e04118e6e2dcf7be01eddba9477ee3
Arosheeba <120004467+Arosheeba@users.noreply.github.com>
2023-03-10 08:59:03 +0530

    Add files via upload
    Hibernate project

---
+++ MavenProject/Application.java
@@ -0,0 +1,8 @@
+package com.build.text.maven.java.MavenProject;
+
+public class Application {
+	static void main( String[] args ){
+		BusManagement busobj = new BusManagement();
+		busobj.toReadInput();
+	}
+}
